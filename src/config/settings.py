6d968b9f76ecc7c9bc8a8db5012a75bc4c27e1a8
Leviinson <votosi197@gmail.com>
2024-02-01 12:27:07 +0100

    updated: DEBUG mode enabled

--- src/config/settings.py
+++ src/config/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 DOMEN = f"http://{os.getenv('NGINX_DOMEN')}"
 
 ALLOWED_HOSTS = [
